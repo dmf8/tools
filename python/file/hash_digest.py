1c1b81ad1289b5bfe4f9e95fbdce6ca195066930
bear08 <957011893@qq.com>
2025-05-22 11:37:21 +0800

    folder batch hash

--- python/file/hash_digest.py
+++ python/file/hash_digest.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
 import sys
+import os
 import hashlib
 import argparse
 
-ap = argparse.ArgumentParser(description="get hash digest of data or file")
+
+def getDataDigest(data, algorithm="sha256"):
+    hash = hashlib.new(algorithm)
+    hash.update(data)
+    return hash.hexdigest()
+
+
+def getFileDigest(file, algorithm="sha256"):
+    with open(file, "rb") as f:
+        return getDataDigest(f.read(), algorithm)
+
+
+ap = argparse.ArgumentParser(description="get hash digest of data/file/folder")
 ap.add_argument("-d", "--data")
 ap.add_argument("-f", "--file")
+ap.add_argument("-p", "--folder")
 ap.add_argument("-a", "--algorithm", default="sha256")
 args = ap.parse_args()
 
 data = args.data
 file = args.file
+folder = args.folder
 algorithm = args.algorithm
 
-if data == None and file == None:
-    print("no data or file provided, exit")
+if data == None and file == None and folder == None:
+    print("no data provided, exit")
     exit()
 
 
-hash = hashlib.new(algorithm)
-
 if data != None:
     raw = data.encode(encoding="utf-8")
-    hash.update(raw)
-    print(f"algorithm {algorithm}, data digest {hash.hexdigest()}")
+    print(f"algorithm {algorithm}, data digest {getDataDigest(raw,algorithm)}")
 elif file != None:
-    with open(file, "rb") as f:
-        hash.update(f.read())
-        print(f"algorithm {algorithm}, file digest {hash.hexdigest()}")
+    print(
+        f"algorithm {algorithm}, file digest {getFileDigest(file,algorithm)}")
+elif folder != None:
+    for dir, folders, files in os.walk(folder):
+        for file in files:
+            path = os.path.join(dir, file)
+            print(
+                f"file: {path}, {algorithm}: {getFileDigest(path,algorithm)}")
